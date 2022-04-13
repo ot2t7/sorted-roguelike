2c675cbe44c20450426198eb26acd36d128ba72e
ot2t7 <ot2t7@riseup.net>
2022-04-13 15:15:35 -0500

    make sure you can tell the difference at roach creation

--- src/systems/debug.rs
+++ src/systems/debug.rs
@@ -11,8 +11,15 @@ impl<'a> System<'a> for DebugPrinter {
     );
 
     fn run(&mut self, (positions, names): Self::SystemData) {
+        let mut entities_counted = 0;
+
         for (name, position) in (&names, &positions).join() {
             println!("{:?} at {:?}.", name, position);
+            entities_counted += 1;
+        }
+
+        if entities_counted == 0 {
+            println!("No entities yet!");
         }
     }
 }
